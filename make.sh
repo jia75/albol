7a244bce251d76aaa0f8caee292cd0972e7b40f3
Jia Hargitt <jiahrgtt@gmail.com>
2025-05-19 21:27:24 +0300

    Fix make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cobc -o bin/out -x -F src/albol.cbl src/view-branch.cbl src/edit-branch.cbl \
+mkdir bin
+cobc -o bin/out -x -free src/albol.cbl src/view-branch.cbl src/edit-branch.cbl \
  src/more-branch.cbl src/add-branch.cbl src/trac-branch.cbl
